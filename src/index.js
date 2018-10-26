89577422c1e4acbedb820e232ed3808d6c91c533
Bruin <819553892@qq.com>
2018-10-26 16:30:18 +0800

    webpack 配置 五：使用less-loader, sass-loader

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import './css/index.css';
+import './less/index.less';
+import './scss/index.scss';
 
 document.write('Hello Webpack!');
--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,14 @@ module.exports = {
 				]
 			},
 			{
+				test: /\.less$/,
+				use: ['style-loader', 'css-loader', 'less-loader']
+			},
+			{
+				test: /\.(scss|sass)$/,
+				use: ['style-loader', 'css-loader', 'sass-loader']
+			},
+			{
 				test: /\.(png|gif|jpe?g|svg)/,
 				use: [
 					{
